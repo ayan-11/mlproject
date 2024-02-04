fc3b376b215ecff27cb7b9372957d72237c3473c
ayan-11 <111264082+ayan-11@users.noreply.github.com>
2024-02-04 11:52:17 +0530

    Hyper Paramater Tuning

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ import pandas as pd
 from src.exceptions import CustomException
 import dill
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 
 
@@ -20,23 +21,33 @@ def save_object(file_path,obj):
     except Exception as e:
         raise CustomException(e,sys)
     
-def evaluate_model(x_train,y_train,x_test,y_test,models):
+def evaluate_models(X_train, y_train,X_test,y_test,models,param):
     try:
-        report={}
+        report = {}
+
         for i in range(len(list(models))):
-            model=list(models.values())[i]
+            model = list(models.values())[i]
+            para=param[list(models.keys())[i]]
 
-            model.fit(x_train,y_train)
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
 
-            y_train_pred=model.predict(x_train)
-            y_test_pred=model.predict(x_test)
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
 
-            train_model_score=r2_score(y_train,y_train_pred)
-            test_model_score=r2_score(y_test,y_test_pred)
+            #model.fit(X_train, y_train)  # Train model
+
+            y_train_pred = model.predict(X_train)
+
+            y_test_pred = model.predict(X_test)
+
+            train_model_score = r2_score(y_train, y_train_pred)
+
+            test_model_score = r2_score(y_test, y_test_pred)
 
             report[list(models.keys())[i]] = test_model_score
 
-            return report
+        return report
 
     except Exception as e:
-        raise CustomException(e,sys)
+        raise CustomException(e, sys)
